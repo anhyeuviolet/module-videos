0c9e6cd7ef169d065fa9fb5b555cdf02e225c43c
KennyNguyen <nguyentiendat713@gmail.com>
2017-01-18 08:12:13 +0700

    Tag 0.2.06

--- modules/videos/version.php
+++ modules/videos/version.php
@@ -5,7 +5,7 @@
  * @Author KENNYNGUYEN (nguyentiendat713@gmail.com)
  * @Website tradacongnghe.com
  * @License GNU/GPL version 2 or any later version
- * @Createdate Thursday, 20 October 2016 08:00:00 GMT
+ * @Createdate Wednesday, 18 January 2017 08:00:00 GMT
  */
 if (! defined('NV_ADMIN') or ! defined('NV_MAINFILE'))
     die('Stop!!!');
@@ -17,8 +17,8 @@ $module_version = array(
     'submenu' => 'user-playlist,user-video,rss,search',
     'is_sysmod' => 0, // 1:0 => Co phai la module he thong hay khong
     'virtual' => 1, // 1:0 => Co cho phep ao hoa module hay khong
-    'version' => '0.2.05', // Phien ban cua module
-    'date' => 'Thursday, 20 October 2016 08:00:00 GMT', // Ngay phat hanh phien ban
+    'version' => '0.2.06', // Phien ban cua module
+    'date' => 'Wednesday, 18 January 2017 08:00:00 GMT', // Ngay phat hanh phien ban
     'author' => 'KENNY NGUYEN (nguyentiendat713@gmail.com)', // Tac gia
     'note' => 'Compatible with NukeViet 4.1', // Ghi chu
     'uploads_dir' => array(
