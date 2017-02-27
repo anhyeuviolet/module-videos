36372013e2ec10f4cfe87c68c497fb12ede822fa
KennyNguyen <nguyentiendat713@gmail.com>
2017-02-27 14:03:27 +0700

    Update to 0.2.07
    + Update JW Player.
    + Update version to 0.2.07.

--- modules/videos/version.php
+++ modules/videos/version.php
@@ -5,21 +5,21 @@
  * @Author KENNYNGUYEN (nguyentiendat713@gmail.com)
  * @Website tradacongnghe.com
  * @License GNU/GPL version 2 or any later version
- * @Createdate Wednesday, 18 January 2017 08:00:00 GMT
+ * @Createdate Monday, 27 February 2017 08:00:00 GMT
  */
 if (! defined('NV_ADMIN') or ! defined('NV_MAINFILE'))
     die('Stop!!!');
 
 $module_version = array(
     'name' => 'Videos', // Tieu de module
-    'modfuncs' => 'main,viewcat,playlists,uploader,player,groups,detail,search,user-playlist,user-video,v_funcs,tag,rss', // Cac function co block
+    'modfuncs' => 'main,viewcat,playlists,uploader,player,groups,detail,search,user-playlist,user-video,tag,rss', // Cac function cho phep chen block
     'change_alias' => 'playlists,groups,uploader,user-playlist,user-video',
     'submenu' => 'user-playlist,user-video,rss,search',
     'is_sysmod' => 0, // 1:0 => Co phai la module he thong hay khong
     'virtual' => 1, // 1:0 => Co cho phep ao hoa module hay khong
-    'version' => '0.2.06', // Phien ban cua module
-    'date' => 'Wednesday, 18 January 2017 08:00:00 GMT', // Ngay phat hanh phien ban
-    'author' => 'KENNY NGUYEN (nguyentiendat713@gmail.com)', // Tac gia
+    'version' => '0.2.07', // Phien ban cua module
+    'date' => 'Monday, 27 February 2017 08:00:00 GMT', // Ngay phat hanh phien ban
+    'author' => 'KENNYNGUYEN (nguyentiendat713@gmail.com)', // Tac gia
     'note' => 'Compatible with NukeViet 4.1', // Ghi chu
     'uploads_dir' => array(
         $module_upload,
